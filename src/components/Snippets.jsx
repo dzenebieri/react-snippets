6b3e8e452941f9be7ace3a22e0b937031a7bc994
Your Name <you@example.com>
2023-11-15 08:02:50 +0400

    git add

--- src/components/Snippets.jsx
+++ src/components/Snippets.jsx
@@ -1,13 +1,13 @@
-import ExtensionInfo from "./extensionInfo"
-import Tables from "./tables/Tables"
-import Components from "./vscode/Components"
+import ExtensionInfo from './ExtensionInfo';
+import Tables from './tables/Tables';
+import Components from './vscode/Components';
 
 export default function Snippets() {
   return (
-    <div className="overflow-y-scroll top-12 bottom-12 absolute w-full">
+    <div className="absolute bottom-12 top-12 w-full overflow-y-scroll">
       <ExtensionInfo />
       <Tables />
       <Components />
     </div>
-  )
+  );
 }
